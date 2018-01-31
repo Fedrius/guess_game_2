9c0500e49814e4c880561e0f7fabaf2a821bb21f
Paul <paulmlee7@gmail.com>
2018-01-31 14:41:08 -0800

    added user input

--- src/components/game.js
+++ src/components/game.js
@@ -5,7 +5,8 @@ class Game extends Component {
         super(props);
 
         this.state = {
-            randomNumber: 0
+            randomNumber: 0,
+            userNumber: ''
         }
 
     }
@@ -22,16 +23,40 @@ class Game extends Component {
         this.generateRandomNumber();
     }
 
+    makeGuess(event){
+        event.preventDefault();
+
+        console.log(this.state.userNumber)
+    }
+
     componentDidMount(){
         this.generateRandomNumber();
     }
 
     render(){
+
+        const btnStyle = {
+            margin: '10px 5px'
+        };
+
+        const {randomNumber, userNumber} = this.state;
+
         console.log(this.state);
         return (
             <div>
-                <p>random num: {this.state.randomNumber}</p>
-                <button onClick={this.resetGame.bind(this)} className='btn orange'>Reset</button>
+                <p>random num: {randomNumber}</p>
+                <div className='row'>
+                    <form className='col s6 offset-s3' onSubmit={this.makeGuess.bind(this)}>
+                        <div className='input-field'>
+                            <input className='center-align' onChange={(event)=> {this.setState({userNumber: event.target.value}) }} value={userNumber} type='number' placeholder='enter a number'/>
+                        </div>
+                        <div className='row center-align'>
+                            <button style={btnStyle} className='btn green'>Guess</button>
+                            <button style={btnStyle} type='button' onClick={this.resetGame.bind(this)} className='btn orange'>Reset</button>
+                            {/*by default button type is submit. change it to button if i dont want it to submit*/}
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
